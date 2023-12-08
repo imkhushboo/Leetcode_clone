b6b8488b9df6593c7ba14329efe6944aef488cd9
imkhushboo <vkhushboo540@gmail.com>
2023-12-08 17:36:20 +0530

    added formik,Yup & Toaster

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -2,6 +2,54 @@ import React, { useState ,useEffect} from 'react';
 import '../CSS/stylesignup.css';
 import { Navigate, useNavigate } from 'react-router-dom';
 import Navbar from './Navbar';
+import { Formik, Form, Field, ErrorMessage, useFormik } from 'formik';
+import * as Yup from 'yup';
+import { Toaster, toast } from 'react-hot-toast';
+
+
+function passwordverify( password) {
+    // console.log(password);
+    var lowerCaseLetters = /[a-z]/g;
+    var upperCaseLetters = /[A-Z]/g;
+    var numbers = /[0-9]/g;
+    var specialchar = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    var error = null ;
+    // console.log(values);
+    if (!password) {
+        error = 'Password Required!!';
+    }
+    else if (password.includes(" ")) {
+        error = 'Invalid Password!';
+    }
+    else if (!password.match(lowerCaseLetters)) {
+        error = "must contain lower case letter";
+    }
+
+    // Validate capital letters
+    else if (!password.match(upperCaseLetters)) {
+        error = "must contain Upper case letter";
+    }
+
+    // Validate numbers
+    else if (!password.match(numbers)) {
+        // console.log(password.match(numbers));
+        error = "must contain numbers";
+
+    }
+
+    // Validate length
+    else if (password.length < 8) {
+        error = "must greater than 8 characters ";
+
+    }
+    else if (!specialchar.test(password)) {
+        error = "must contain special character!";
+    }
+    return error;
+
+
+}
+
 
 
 function Login() {
@@ -9,6 +57,58 @@ function Login() {
     const [password,setPassword] = useState();
     const [token,setToken] = useState();
     const navigate = useNavigate();
+
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+       
+        validationSchema: Yup.object().shape({
+            email: Yup.string()
+            .email("Invalid email address")
+            .required("Required"),
+            password:Yup.string().required("Required").test(function(values,error)
+            {
+            //    console.log(passwordverify(values));
+               const msg = passwordverify(values);
+              
+               if(msg === null)
+               {
+                return true;
+               }
+              return error.createError({message:msg});
+            })
+        }),
+        onSubmit:async(values) => {
+            values = Object.assign(values);
+            console.log(values);
+            let response = await loginUser({email: values.email,password: values.password});
+            console.log(response);
+            if(response.status === 200)
+            {
+                toast.success("Login  Successfully!");
+                localStorage.setItem("token",response.token);
+                setToken(response.token);
+                console.log(response.token);
+                setTimeout(()=>{
+                    navigate('/problemSet/all');
+                    navigate(0);
+                },1000);
+               
+            }
+            else if(response.status === 500)
+            {
+                toast.error("This didn't work.")
+    
+                    toast.error(response.message);
+            }
+            else{
+                toast.loading('Signing up ...!');
+            }  
+    // handle submission
+        },
+    });
 
     useEffect(()=>{
         if(localStorage.getItem('token'))
@@ -19,7 +119,7 @@ function Login() {
        
     },[])
 
-    const loginUser= async()=>{
+    const loginUser= async({email,password})=>{
        
         try{
             const response= await fetch("http://localhost:3001/login",{
@@ -29,7 +129,7 @@ function Login() {
             },
 
             body : JSON.stringify({
-                email ,password
+                email,password
             })
         });
 
@@ -41,34 +141,66 @@ function Login() {
             const err = json.msg;
             throw err;
         }
-        localStorage.setItem("token",json.token);
-        setToken(json.token);
-        console.log(json.token);
-        navigate('/problemSet/all');
-        navigate(0);
+        return {status:200,token:json};
+        
 
         }catch(err)
         {  
           alert(err);
           console.log(err);
+          return {status:500,message:err};
         }
 
     }
   return (
       <div className='login'>
-         <div className='form'>
+        <Toaster toastOptions={{
+       className: 'toaster'}}
+      position="top-center"
+      reverseOrder={false}
+     />
+        <Formik>
+         <Form className='form' onSubmit={formik.handleSubmit}>
         <div className ="detail"> 
         <label htmlFor="email">Email : </label>
-        <input type="Email" placeholder='Email Address'  onChange={(e)=>{setEmail(e.target.value)}}/>
+        <div className="detail-input">
+        <Field id="email" type="email" placeholder='Email Address'  onBlur={formik.handleBlur} value={formik.values.email}  onChange={formik.handleChange} />
+        {formik.touched.email?
+        <div className="error">
+                    {formik.errors.email ? 
+                     <i id="tooltip-error" className="fa-solid fa-circle-xmark"> <span className='tooltiptext-error'>{formik.errors.email}</span></i>:
+                     <i id="tooltip-correct"className="fa-solid fa-circle-check"></i>}
+
+        </div>:
+        null
+         
+        }
+
+        </div>
+       
         </div>
         <div className ="detail">
         <label htmlFor="password" >Password : </label>
-        <input type="Password" placeholder='Password' onChange={(e)=>{setPassword(e.target.value)}}/>
+        <div className="detail-input">
+        <Field id="password" type="password" placeholder='Enter Password'  onBlur={formik.handleBlur} value={formik.values.password}  onChange={formik.handleChange} />
+        {formik.touched.email?
+        <div className="error">
+                    {formik.errors.password ? 
+                     <i id="tooltip-error" className="fa-solid fa-circle-xmark"> <span className='tooltiptext-error'>{formik.errors.password}</span></i>:
+                     <i id="tooltip-correct"className="fa-solid fa-circle-check"></i>}
+
+        </div>:
+        null
+         
+        }
+
+        </div>
         </div>
         <div className='btn'>
-        <button onClick={loginUser} type='submit'> Login </button>
+        <button type='submit'> Login </button>
         </div>
-        </div>
+        </Form>
+        </Formik>
       
     </div>
   )
--- client/src/Components/SignUp.jsx
+++ client/src/Components/SignUp.jsx
@@ -2,6 +2,54 @@ import React, { useState } from 'react'
 import '../CSS/stylesignup.css';
 import { Navigate, useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import { Formik, Form, Field, ErrorMessage, useFormik } from 'formik';
+import * as Yup from 'yup';
+import { Toaster, toast } from 'react-hot-toast';
+
+
+function passwordverify( password) {
+    // console.log(password);
+    var lowerCaseLetters = /[a-z]/g;
+    var upperCaseLetters = /[A-Z]/g;
+    var numbers = /[0-9]/g;
+    var specialchar = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    var error = null ;
+    // console.log(values);
+    if (!password) {
+        error = 'Password Required!!';
+    }
+    else if (password.includes(" ")) {
+        error = 'Invalid Password!';
+    }
+    else if (!password.match(lowerCaseLetters)) {
+        error = "must contain lower case letter";
+    }
+
+    // Validate capital letters
+    else if (!password.match(upperCaseLetters)) {
+        error = "must contain Upper case letter";
+    }
+
+    // Validate numbers
+    else if (!password.match(numbers)) {
+        // console.log(password.match(numbers));
+        error = "must contain numbers";
+
+    }
+
+    // Validate length
+    else if (password.length < 8) {
+        error = "must greater than 8 characters ";
+
+    }
+    else if (!specialchar.test(password)) {
+        error = "must contain special character!";
+    }
+    return error;
+
+
+}
+
 
 
 function SignUp() {
@@ -9,60 +57,215 @@ function SignUp() {
     const [email ,setEmail ] = useState();
     const [password ,setPassword] = useState();
 
-   const signupUser = async ()=>{
-    try{
+    const formik = useFormik({
+        initialValues: {
+            "email":"",
+            "password":"",
+            "confirmPassword":""
+    },
+    validationSchema: Yup.object().shape({
+        email: Yup.string()
+        .email("Invalid email address")
+        .required("Required"),
+        password:Yup.string().required("Required").test(function(values,error)
+        {
+        //    console.log(passwordverify(values));
+           const msg = passwordverify(values);
+          
+           if(msg === null)
+           {
+            return true;
+           }
+          return error.createError({message:msg});
+        }),
+        confirmPassword: Yup.string().required("Required").oneOf([Yup.ref('password'), null], 'Passwords must match')
+    }),
+    onSubmit: async values => {
+        // console.log(values);
+        values = Object.assign(values);
+        console.log(values);
+        let response = await signupUser({email: values.email,password: values.password});
+        console.log(response);
+        // toast.error("This didn't work.")
+            if(response.status === 200)
+            {
+                toast.success("Signed Up Successfully!");
+                 
+                setTimeout(()=>{
+                    navigate('/login')
+                },100);
+            }
+            else if(response.status === 500)
+            {
+                toast.error("This didn't work.")
+    
+                    toast.error(response.message);
+            }
+            else{
+                toast.loading('Signing up ...!');
+            } 
         
-        const response= await fetch("http://localhost:3001/signup",{
+      }});
+
+   const signupUser = async ({email,password})=>{
+    try{
+        console.log(email,password);
+        
+         const response=  await fetch("http://localhost:3001/signup",{
             method : 'POST',
             headers: {
                 "Content-Type": "application/json",
             },
 
             body : JSON.stringify({
-                email ,password
+                email,password
             })
         });
 
         
-        const json = await response.json();
+        const json= await response.json();
+        
         if(response.status !=200)
         {
             const err = json.msg;
             throw err;
         }
-        navigate('/login');
+        // setTimeout(()=>{
+        //     navigate('/login');
+        // },1000);
+
+        return {status:200,message:'SuccessFull!!'}
+        
+        
     }
     catch(err)
     {
-          console.log(err);
-        
+          return {status:500,message:err};
    }
 }
    
 
   return (
+    <>
+   
+
     <div className='signup'>
-         <div className='form'>
+    <Toaster
+toastOptions={{
+  className: '',
+  style: {
+    border: '1px solid #713200',
+    padding: '16px',
+    maxHeight:'10%',
+    display:'flex',
+    color:'red',
+    flexDirection:"column",
+  },
+}}
+/>
+      
+        <Formik >
+        <Form className='form' onSubmit={formik.handleSubmit}>
         <div className ="detail"> 
         <label htmlFor="email">Email : </label>
-        <input type="Email" placeholder='Email Address'  onChange={(e)=>{setEmail(e.target.value)}}/>
+        <div className='detail-input'>
+        <Field
+               id="email"
+               type="email"
+               name="email"
+               placeholder="Enter email address"
+               onChange={formik.handleChange}
+               value={formik.values.email}
+               onBlur={formik.handleBlur}
+             />
+        {
+        (formik.touched.email) ? 
+        <div className='error'> 
+        {
+            (formik.errors.email)?
+                                   <i id="tooltip-error" className="fa-solid fa-circle-xmark"> <span className='tooltiptext-error'>{formik.errors.email}</span></i>:
+                                   <i id="tooltip-correct"className="fa-solid fa-circle-check"></i>
+                                   
+        }
+        </div>: 
+        null
+        
+        }
+        
+        </div>
+       
+
+       
         </div>
         <div className ="detail">
         <label htmlFor="password" >Password : </label>
-        <input type="Password" placeholder='Password' onChange={(e)=>{setPassword(e.target.value)}}/>
+        <div className="detail-input">
+        <Field
+               id="password"
+               type="password"
+               name="password"
+               placeholder="enter password"
+               onChange={formik.handleChange}
+               value={formik.values.password}
+               onBlur={formik.handleBlur}
+               
+             />
+
+        {formik.touched.password?
+         <div className="error">
+         { (formik.errors.password)?
+          <i id="tooltip-error" className="fa-solid fa-circle-xmark"> <span className='tooltiptext-error'>{formik.errors.password}</span></i>:
+          <i id="tooltip-correct"className="fa-solid fa-circle-check"></i>
+          }
+
+   </div>:
+        null
+       
+        }
+        
+
+        </div>
+        
+
         </div>
         <div className ="detail"> 
-        <label htmlFor="confirm password" >Confirm Password :</label>
-        <input type="Password" placeholder='confirm password' />
+        <label htmlFor="confirmPassword" >Confirm Password :</label>
+        <div className='detail-input'>
+        <Field
+               id="confirmPassword"
+               type="password"
+               name="confirmPassword"
+               placeholder="confirm password"
+               onChange={formik.handleChange}
+               value={formik.values.confirmPassword}
+               onBlur={formik.handleBlur}
+             />
+
+        {formik.touched.confirmPassword?
+         <div className="error">
+         {formik.errors.confirmPassword?
+         <i id="tooltip-error" className="fa-solid fa-circle-xmark"> <span className='tooltiptext-error'>{formik.errors.confirmPassword}</span></i>:
+         <i id="tooltip-correct"className="fa-solid fa-circle-check"></i>}
+
+          </div>:
+           null
+       
+        }
+
+
+        </div>
+        
         </div>
         <div className='btn'>
-        <button onClick={signupUser} type='submit'> Create Account</button>
+        <button   disabled={    Object.keys(formik.errors).length ===0 ?false:true} type='submit'> Create Account</button>
         <p>Already have a account? <Link to='/login'>Login</Link></p>
         </div>
-      
-      
+        </Form>
+
+        </Formik>
+ 
     </div>
-    </div>
+    </>
    
   )
 }
