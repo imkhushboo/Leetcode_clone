19598bdd8998aeb923123bfbfaab869c2bd737f9
imkhushboo <vkhushboo540@gmail.com>
2023-12-26 17:23:48 +0530

    Upated app using lazy loading concept

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,22 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-import Home from './Components/Home';
-import Page from './Components/Page';
-import SignUp from "./Components/SignUp";
-import Navbar from "./Components/Navbar";
-import Login from "./Components/Login";
-import Submission from "./Components/Submission";
+import React, { Suspense, lazy } from "react";
+
+
+const Home =  lazy(()=> import ('./Components/Home'));
+const Page =  lazy(()=> import('./Components/Page'));
+const SignUp =  lazy(()=> import ("./Components/SignUp"));
+const Navbar =  lazy(()=>import("./Components/Navbar"))
+const Login =  lazy(()=>import("./Components/Login")); 
+const Submission=  lazy(()=>import("./Components/Submission"));
 import './App.css';
-import Me from "./Components/Me";
-import Loginprotected from "./middleware/Loginprotected";
-import Blog from "./Components/Blog";
-import Allsubmission from "./Components/Allsubmission";
-import Pagenotfound from "./Components/Pagenotfound";
-import { Toaster } from "react-hot-toast";
+const Me =  lazy(()=>import ("./Components/Me"));
+const Loginprotected =  lazy(()=>import("./middleware/Loginprotected")); 
+const  Blog =  lazy(()=>import("./Components/Blog")) 
+const Allsubmission =  lazy(()=>import("./Components/Allsubmission")) 
+const Pagenotfound =  lazy(()=>import("./Components/Pagenotfound")) 
+import { Toaster }  from "react-hot-toast";
+
+
 
 
 
@@ -19,6 +24,8 @@ function App() {
   
   return(
     <BrowserRouter>
+    <Suspense fallback={<div>Loading...</div>}>
+
     <Toaster  autoClose={4000}/>
 
     <Navbar />
@@ -34,6 +41,7 @@ function App() {
     <Route exact path="/submissions" element={<Loginprotected><Allsubmission /></Loginprotected>} ></Route>
     <Route exact path='/pagenotfound' element={<Pagenotfound/>} />
     </Routes>
+    </Suspense>
     </BrowserRouter> 
 
   
